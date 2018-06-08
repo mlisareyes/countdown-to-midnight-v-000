9afc203c73251106a958ec781778d48daf2fca3c
Monica Lisa Reyes <mlisareyes@gmail.com>
2018-06-08 05:44:47 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,9 @@
 #write your code here
 
-def countdown
+def countdown(seconds_to_midnight)
+  while seconds_to_midnight > 0
+    puts "#{seconds_to_midnight} SECONDS(S)!"
+    seconds_to_midnight -= 1
+  end
+  "HAPPY NEW YEAR!"
 end
